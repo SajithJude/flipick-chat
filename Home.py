1cd7e4663b699dbfc949748aea9b815e72428dc8
Jude Sajith <judesajith.aj@gmail.com>
2023-03-20 04:25:16 -0400

    ssss

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import openai 
 from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader,PromptHelper
-import os
+import os 
 openai.api_key = os.getenv("API_KEY")
 
 # Loading from a directory
@@ -10,3 +10,6 @@ index = GPTSimpleVectorIndex(documents)
 
 
 st.write(index)
+inpt = st.text_area("Ask something")
+response = index.query(inpt)
+st.write(response)

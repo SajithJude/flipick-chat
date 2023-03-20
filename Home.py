7686adfee9fb8637f3bf2daa445ed6fc6482419f
Jude Sajith <judesajith.aj@gmail.com>
2023-03-20 03:44:28 -0400

    sss

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import openai 
+from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader,PromptHelper
+import os
+
+
+# Loading from a directory
+# documents = SimpleDirectoryReader('content').load_data()
+
+st.write("hello world")

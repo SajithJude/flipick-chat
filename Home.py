d3d925301e485fdd7e8324198f1162ec36f39e07
Jude Sajith <judesajith.aj@gmail.com>
2023-03-20 05:10:09 -0400

    sss

--- Home.py
+++ Home.py
@@ -2,7 +2,23 @@ import streamlit as st
 import openai 
 from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader,PromptHelper
 import os 
+from streamlit_chat import message as st_message
+
 openai.api_key = os.getenv("API_KEY")
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+def generate_answer():
+    tokenizer, model = get_models()
+    user_message = st.session_state.input_text
+    message_bot = index.query(user_message)
+    st.session_state.history.append({"message": user_message, "is_user": True})
+    st.session_state.history.append({"message": message_bot, "is_user": False})
+
+
+
+
 
 # Loading from a directory
 documents = SimpleDirectoryReader('content').load_data()
@@ -11,9 +27,7 @@ index.save_to_disk('index.json')
 # load from disk
 index = GPTSimpleVectorIndex.load_from_disk('index.json')
 
-# st.write(index)
-inpt = st.text_area("Ask something")
+st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
 
-if inpt:
-    response = index.query(inpt)
-    st.write(response)
+for chat in st.session_state.history:
+    st_message(**chat) 

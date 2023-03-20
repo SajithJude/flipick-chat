0be289153d19f8ef4144d2bb349f0582bfc4e83d
Jude Sajith <judesajith.aj@gmail.com>
2023-03-20 06:03:12 -0400

    sss

--- Home.py
+++ Home.py
@@ -5,6 +5,37 @@ import os
 from streamlit_chat import message as st_message
 
 
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+</div>
+"""
+
+
+
 favicon = "logo_flipick_colored.png"
 
 st.set_page_config(page_title="Flipick Chat", page_icon=favicon)
@@ -38,3 +69,7 @@ st.text_input("Ask flipick bot a question", key="input_text", on_change=generate
 
 for chat in st.session_state.history:
     st_message(**chat) 
+
+
+
+st.markdown(footer,unsafe_allow_html=True)

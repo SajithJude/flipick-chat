01c95fb9ff9f675db73c9ae0d2b22b58cb1a4517
Jude Sajith <judesajith.aj@gmail.com>
2023-03-20 04:27:20 -0400

    ssss

--- Home.py
+++ Home.py
@@ -7,7 +7,9 @@ openai.api_key = os.getenv("API_KEY")
 # Loading from a directory
 documents = SimpleDirectoryReader('content').load_data()
 index = GPTSimpleVectorIndex(documents)
-
+index.save_to_disk('index.json')
+# load from disk
+index = GPTSimpleVectorIndex.load_from_disk('index.json')
 
 st.write(index)
 inpt = st.text_area("Ask something")

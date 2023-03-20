788e107ac16e14e32caf5781b898f1c4ed05ec22
Jude Sajith <judesajith.aj@gmail.com>
2023-03-20 07:57:02 -0400

    sss

--- Home.py
+++ Home.py
@@ -101,6 +101,7 @@ st.sidebar.image("logo_flipick_colored.png")
 #         for i in range(len(conversation_history)):
 #             st.button(f"Conversation {i+1}")
 # Add a text input and a save button
+st.image("Flipick_Logo-1.jpg")
 input_text = st.text_input("Ask flipick bot a question", key="input_text", on_change=generate_answer)
 st.caption("Disclaimer : This ChatBOT is a pilot built solely for the purpose of a demo to Indian Institute of Banking and Finance (IIBF). The BOT has been trained based on the book Treasury Management published by IIBF. All content rights vest with IIBF")
 

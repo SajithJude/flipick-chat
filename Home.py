442a6135b9798308284f09582e1474099001b0af
Jude Sajith <judesajith.aj@gmail.com>
2023-03-25 14:37:11 -0400

    sjsjsjs

--- Home.py
+++ Home.py
@@ -1,21 +1,46 @@
 import streamlit as st
+from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader, QuestionAnswerPrompt
+import os
+import PyPDF2
+
+# import streamlit as st
 import openai 
-from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader,PromptHelper,QuestionAnswerPrompt
-import os 
+# from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader,PromptHelper,QuestionAnswerPrompt
+# import os 
 from streamlit_chat import message as st_message
 
 favicon = "favicon.ac8d93a.69085235180674d80d902fdc4b848d0b.png"
 st.set_page_config(page_title="Flipick Chat", page_icon=favicon)
 
 openai.api_key = os.getenv("API_KEY")
+# Define function to extract text from PDF
+def extract_text(file):
+    pdf_reader = PyPDF2.PdfReader(file)
+    text = ""
+    for page in range(len(pdf_reader.pages)):
+        text += pdf_reader.pages[page].extract_text()
+    return text
 
-try:
-    index = GPTSimpleVectorIndex.load_from_disk('index.json')
-except FileNotFoundError:
-    # Loading from a directory
-    documents = SimpleDirectoryReader('content').load_data()
+# Define function to create and save the index
+def create_index(index_file):
+    # Load the index file
+    index = GPTSimpleVectorIndex.load_from_disk(index_file)
+
+    return index
+
+def createindex(pdf_file):
+    # Load the PDF file and extract text
+    text = extract_text(pdf_file)
+
+    # Create documents
+    documents = [Document(text)]
+
+    # Create and save the index
+    filename = pdf_file.name.split(".")[0] + ".json"
     index = GPTSimpleVectorIndex(documents)
-    index.save_to_disk('index.json')
+    index.save_to_disk(filename)
+
+    return filename
 
 if "history" not in st.session_state:
     st.session_state.history = []
@@ -28,7 +53,7 @@ def generate_answer():
         st.session_state.history.append({"message": "I'm sorry, I'm not allowed to perform calculations.", "is_user": False})
     else:
         query_str = str(user_message)
-        context_str = "All answers provided should be based only on the document in the index."
+        context_str = "Generate answers for the questions that are relevant only to the documents context, throw a default answer saying I dont know for unrelevant questions."
         QA_PROMPT_TMPL = (
             "We have provided context information below. \n"
             "---------------------\n"
@@ -37,12 +62,40 @@ def generate_answer():
             "Given this information, please answer the question: {query_str}\n"
         )
         QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
-        message_bot = index.query(query_str, text_qa_template=QA_PROMPT)
+        message_bot = index.query(query_str, text_qa_template=QA_PROMPT, response_mode="compact", mode="embedding")
+        # source = message_bot.get_formatted_sources()
+        # st.sidebar.write("Answer Source :",source)  # added line to display source on sidebar
         st.session_state.history.append({"message": user_message, "is_user": True})
         st.session_state.history.append({"message": str(message_bot), "is_user": False})
 
-col1, col2 = st.columns([3.2, 1])
-col2.image("Flipick_Logo-1.jpg", width=160)
+# Create the app layout
+# st.title("PDF Indexer")
+with st.expander("Upload pdf and select index"):
+    pdf_file = st.file_uploader("Upload a PDF file")
+
+    # If a PDF file is uploaded, create and save the index
+    if pdf_file:
+        filename = createindex(pdf_file)
+        st.write(filename)
+        st.success(f"Index saved to {filename}")
+
+
+    # List all json files in the directory
+    files = os.listdir('.')
+    json_files = [f for f in files if f.endswith('.json')]
+
+    # Create a dropdown to select the index file
+    index_file = st.selectbox("Select an index file:", json_files)
+
+    # If an index file is selected, create the index
+    if index_file:
+        index = create_index(index_file)
+        st.success(f"Index loaded from {index_file}")
+    else:
+        st.warning("No index file selected.")
+
+col1, col2 = st.columns([1.4, 1])
+col2.image("Flipick_Logo-1.jpg", width=210)
 st.write("")
 st.write("")
 

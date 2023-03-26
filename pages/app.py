ec1fa8563de2fe94355f1cd089a0acfd20b08f40
Jude Sajith <judesajith.aj@gmail.com>
2023-03-26 13:25:24 -0400

    ddd

--- pages/app.py
+++ pages/app.py
@@ -9,3 +9,23 @@ from streamlit_chat import message as st_message
 
 
 
+
+# expander = st.expander("Upload pdfs and create index")
+# pdf_files = expander.file_uploader("Upload PDFs", accept_multiple_files=True)
+pdf_files = st.file_uploader("Upload PDF files", accept_multiple_files=True)
+if pdf_files:
+    # Process the PDF files and create the index
+    with st.spinner('Uploading file...'):
+        directory_path = "content/"
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+        for pdf_file in pdf_files:
+            with open(os.path.join(directory_path, pdf_file.name), "wb") as f:
+                f.write(pdf_file.getbuffer())
+
+    st.success(f"PDF files successfully uploaded to path {directory_path}. Creating index...")
+    with st.spinner("It will take a few Minutes to index the book, Please wait"):
+        documents = SimpleDirectoryReader('content').load_data()
+        index = GPTSimpleVectorIndex(documents)
+        index.save_to_disk('index.json')
+        st.success("Index created successfully.")

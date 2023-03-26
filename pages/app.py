c08812dbc9d51b529aa0173d65e745767f54c8c5
Jude Sajith <judesajith.aj@gmail.com>
2023-03-26 12:56:18 -0400

    ssss

--- pages/app.py
+++ pages/app.py
@@ -9,32 +9,3 @@ from streamlit_chat import message as st_message
 
 
 
-pdf = st.file_uploader("Upload PDF file", type="pdf")
-if pdf:
-    with open(os.path.join("content", pdf.name), "wb") as f:
-        f.write(pdf.getbuffer())
-
-index_path = os.path.join("content", "index.json")
-if not os.path.exists(index_path):
-    st.warning("Please upload a PDF file to create the index")
-    index = None
-else:
-    reader = SimpleDirectoryReader("content").load_data()
-    # documents = [Document(filename, open(filename, "rb").read()) for filename in reader.get_filenames()]
-    index = GPTSimpleVectorIndex(reader)
-    index.save_to_disk('content/index.json')
-    st.success("Index created successfully.")
-
-
-# if index is not None:
-#     index = GPTSimpleVectorIndex(documents)
-#     index.save_to_disk('index.json')
-#     st.success("Index created successfully.")
-
-# question_answerer = QuestionAnswerPrompt(index)
-
-# def chatbot(message):
-#     response = question_answerer.answer(message)
-#     st_message(response)
-
-# st.chatbot(callback=chatbot)

eea84c8f008668f0b2704e3ebcdcd3e61c63442c
Jude Sajith <judesajith.aj@gmail.com>
2023-03-26 12:41:13 -0400

    sss

---
+++ pages/app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader, QuestionAnswerPrompt
+import os
+import PyPDF2
+
+import openai 
+
+from streamlit_chat import message as st_message
+
+
+
+pdf_file = st.file_uploader("Upload PDF file", type="pdf")
+if pdf_file:
+    with open(os.path.join("content", pdf_file.name), "wb") as f:
+        f.write(pdf_file.getbuffer())
+
+index_path = os.path.join("content", "index.json")
+if not os.path.exists(index_path):
+    st.warning("Please upload a PDF file to create the index")
+    index = None
+else:
+    reader = SimpleDirectoryReader("content", extension="pdf")
+    documents = [Document(filename, open(filename, "rb").read()) for filename in reader.get_filenames()]
+    index = GPTSimpleVectorIndex(documents)
+    index.save(index_path)
+    st.success("Index created")
+
+if index:
+    index.load(index_path)
+    st.success("Index loaded")
+
+question_answerer = QuestionAnswerPrompt(index)
+
+def chatbot(message):
+    response = question_answerer.answer(message)
+    st_message(response)
+
+st.chatbot(callback=chatbot)

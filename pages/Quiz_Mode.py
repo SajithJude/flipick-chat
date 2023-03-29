cb347b4a429321a02bd4654e519cd7a9316de3dc
Jude Sajith <judesajith.aj@gmail.com>
2023-03-29 12:53:55 -0400

    ssss

--- pages/Quiz_Mode.py
+++ pages/Quiz_Mode.py
@@ -4,12 +4,11 @@ from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationEntityMemory
 from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
 from langchain.llms import OpenAI
+from llama_index import GPTSimplevectorIndex
 import os 
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 
 # Set Streamlit page configuration
 st.set_page_config(page_title='🧠MemoryBot🤖', layout='wide')
-
 # Initialize session states
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
@@ -19,6 +18,8 @@ if "input" not in st.session_state:
     st.session_state["input"] = ""
 if "stored_session" not in st.session_state:
     st.session_state["stored_session"] = []
+if "index" not in st.session_state:
+    st.session_state["index"] = None
 
 # Define function to get user input
 def get_text():
@@ -61,6 +62,11 @@ with st.sidebar.expander("🛠️ ", expanded=False):
             st.session_state.entity_memory.buffer
     MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','text-davinci-003','text-davinci-002','code-davinci-002'])
     K = st.number_input(' (#)Summary of prompts to consider',min_value=3,max_value=1000)
+    # Option to load an index
+    if st.checkbox("Load Index"):
+        index_path = st.file_uploader("Select an index file", type="json")
+        if index_path is not None:
+            st.session_state.index = GPTSimplevectorIndex(index_path)
 
 # Set up the Streamlit app layout
 st.title("🤖 Chat Bot with 🧠")
@@ -79,72 +85,54 @@ llm = OpenAI(temperature=0,
 
 # Create a ConversationEntityMemory object if not already created
 if 'entity_memory' not in st.session_state:
-        st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K )
-    
-# Create the
+    st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K )
+
 # Create the ConversationChain object with the specified configuration
 Conversation = ConversationChain(
-        llm=llm, 
-        prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-        memory=st.session_state.entity_memory
-    )
+    llm=llm, 
+    prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+    memory=st.session_state.entity_memory
+)  
 
-# Create an instance of GPTSimpleVectorIndex using uploaded documents
-def create_index(documents):
-    index = GPTSimpleVectorIndex(documents)
-    return index
-
-uploaded_files = st.file_uploader("Upload one or more documents to answer questions", accept_multiple_files=True)
-
-if uploaded_files:
-    documents = []
-    for file in uploaded_files:
-        if file.name.endswith('.txt'):
-            documents.append(file.read().decode('utf-8'))
-    if documents:
-        index = create_index(documents)
-        question = get_text()
-        if question:
-            response = index.query(question)[0]['answer']
-            st.write(response)
-            st.session_state.past.append(question)
-            st.session_state.generated.append(response)
-    else:
-        st.warning('Please upload a text file')
-else:
-    # Add a button to start a new chat
-    st.sidebar.button("New Chat", on_click = new_chat, type='primary')
-
-    # Get the user input
+# Generate the output using the ConversationChain object and the user input, and add the input/output to the session
+if st.session_state.index is not None:
     user_input = get_text()
-
-    # Generate the output using the ConversationChain object and the user input, and add the input/output to the session
+    if user_input:
+        response = st.session_state.index.search(user_input)
+        if response is not None:
+            output = response
+            st.session_state.past.append(user_input)  
+            st.session_state.generated.append(output)
+else:
+    user_input = get_text()
     if user_input:
         output = Conversation.run(input=user_input)  
         st.session_state.past.append(user_input)  
         st.session_state.generated.append(output)  
 
-    # Allow to download as well
-    download_str = []
-    # Display the conversation history using an expander, and allow the user to download it
-    with st.expander("Conversation", expanded=True):
-        for i in range(len(st.session_state['generated'])-1, -1, -1):
-            st.info(st.session_state["past"][i],icon="🧐")
-            st.success(st.session_state["generated"][i], icon="🤖")
-            download_str.append(st.session_state["past"][i])
-            download_str.append(st.session_state["generated"][i])
+# Allow to download as well
+download_str = []
+# Display the conversation history using an expander, and allow the user to download it
+with st.expander("Conversation", expanded=True):
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        st.info(st.session_state["past"][i],icon="🧐")
+        st.success(st.session_state["generated"][i], icon="🤖")
+        download_str.append(st.session_state["past"][i])
+        download_str.append(st.session_state["generated"][i])
+    
+    # Can throw error - requires fix
+    download_str = '\n'.join(download_str)
+    if download_str:
+        st.download_button('Download',download_str)
 
-        # Can throw error - requires fix
-        download_str = '\n'.join(download_str)
-        if download_str:
-            st.download_button('Download',download_str)
+# Display stored conversation sessions in the sidebar
+for i, sublist in enumerate(st.session_state.stored_session):
+        with st.sidebar.expander(label= f"Conversation-Session:{i}"):
+            st.write(sublist)
 
-    # Display stored conversation sessions in the sidebar
-    for i, sublist in enumerate(st.session_state.stored_session):
-            with st.sidebar.expander(label= f"Conversation-Session:{i}"):
-                st.write(sublist)
+# Allow the user to clear all stored conversation sessions
+if st.session_state.stored_session:   
+    if st.sidebar.checkbox("Clear-all"):
+        del st.session_state.stored_session
 
-    # Allow the user to clear all stored conversation sessions
-    if st.session_state.stored_session:   
-        if st.sidebar.checkbox("Clear-all"):
-            del st.session_state.stored_session
+           

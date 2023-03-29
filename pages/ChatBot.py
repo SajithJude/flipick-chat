bc0b3afc4d750371ed0c38ecf4b4f823136944f6
Jude Sajith <judesajith.aj@gmail.com>
2023-03-29 19:20:07 -0400

    ssss

--- pages/ChatBot.py
+++ pages/ChatBot.py
@@ -6,14 +6,20 @@ import openai
 
 openai.api_key = os.getenv("API_KEY")
 
-index_set = {}
-service_context = ServiceContext.from_defaults(chunk_size_limit=512)
-
 content_dir = Path("content")
 for pdf_file in content_dir.glob("*.pdf"):
     file_path = str(pdf_file.resolve())
-    file_name = pdf_file.stem
-    cur_index = GPTSimpleVectorIndex.from_documents({file_name: file_path}, service_context=service_context)
-    index_set[file_name] = cur_index
-    cur_index.save_to_disk(f'index_{file_name}.json')
-    st.write(f'index_{file_name}.json')
+    st.write(file_path)
+
+
+# index_set = {}
+# service_context = ServiceContext.from_defaults(chunk_size_limit=512)
+
+# content_dir = Path("content")
+# for pdf_file in content_dir.glob("*.pdf"):
+#     file_path = str(pdf_file.resolve())
+#     file_name = pdf_file.stem
+#     cur_index = GPTSimpleVectorIndex.from_documents({file_name: file_path}, service_context=service_context)
+#     index_set[file_name] = cur_index
+#     cur_index.save_to_disk(f'index_{file_name}.json')
+#     st.write(f'index_{file_name}.json')

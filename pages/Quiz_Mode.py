ef7fa56ecfd7ed87d9503c1f705d5e09d880ff5e
Jude Sajith <judesajith.aj@gmail.com>
2023-03-29 11:27:06 -0400

    ssss

--- pages/Quiz_Mode.py
+++ pages/Quiz_Mode.py
@@ -4,7 +4,7 @@ import io
 import streamlit as st
 # from dotenv import load_dotenv
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores.faiss import FAISS
+from langchain.vectorstores import FAISS
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.llms import OpenAI
 from langchain.chains import ChatVectorDBChain

d4754dc100853bdf3dd81b1410fb4d439af0c795
Jude Sajith <judesajith.aj@gmail.com>
2023-03-29 16:18:17 -0400

    ssss

--- pages/chain.py
+++ pages/chain.py
@@ -1,4 +1,4 @@
-from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
+from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma, Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -6,59 +6,44 @@ import pinecone
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 import openai
-import os 
-import streamlit as st 
+import os
+import streamlit as st
 
+# Load the data and split it into chunks
+@st.cache
+def load_and_split_data():
+    loader = UnstructuredPDFLoader("content/Treasury Management Book .pdf")
+    data = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(data)
+    return texts
 
-# loader = UnstructuredPDFLoader("content/Treasury Management Book .pdf")
-# data = loader.load()
-# st.write(f'You have {len(data)} document(s) in your data')
-# st.write(f'There are {len(data[0].page_content)} characters in your document')
-
-
-
-
-# Check if the texts are already stored in the session state
-if 'texts' not in st.session_state:
-  # Load the data and split it into chunks
-  loader = UnstructuredPDFLoader("content/Treasury Management Book .pdf")
-  data = loader.load()
-  st.write(f'You have {len(data)} document(s) in your data')
-  st.write(f'There are {len(data[0].page_content)} characters in your document')
-
-  text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-  texts = text_splitter.split_documents(data)
-  # Save the texts to the session state
-  st.session_state['texts'] = texts
-else:
-  # Load the texts from the session state
-  texts = st.session_state['texts']
+texts = load_and_split_data()
 
 openai.api_key = os.getenv("API_KEY")
 
+# Embeddings and Pinecone index
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def prepare_embeddings_and_pinecone_index():
+    embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key)
+    pinecone.init(
+        api_key="ef6b0907-1e0f-4b7e-a99d-b893c5686680",
+        environment="eu-west1-gcp"
+    )
+    index_name = "langchain-openai"
+    namespace = "book"
+    docsearch = Pinecone.from_texts(
+        [t.page_content for t in texts], embeddings,
+        index_name=index_name, namespace=namespace
+    )
+    return embeddings, docsearch
 
-
-embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key)
-
-# initialize pinecone
-pinecone.init(
-    api_key="ef6b0907-1e0f-4b7e-a99d-b893c5686680",  # find at app.pinecone.io
-    environment="eu-west1-gcp" # next to api key in console
-)
-index_name = "langchain-openai"
-namespace = "book"
-
-docsearch = Pinecone.from_texts(
-  [t.page_content for t in texts], embeddings,
-  index_name=index_name, namespace=namespace)
-
+embeddings, docsearch = prepare_embeddings_and_pinecone_index()
 
 llm = OpenAI(temperature=0, openai_api_key=openai.api_key)
 chain = load_qa_chain(llm, chain_type="stuff")
 
-query =st.text_input("Input question")
+query = st.text_input("Input question")
 if query:
-    docs = docsearch.similarity_search(query,
-    include_metadata=True, namespace=namespace)
-
+    docs = docsearch.similarity_search(query,include_metadata=True, namespace=namespace)
     st.write(chain.run(input_documents=docs, question=query))

b96394777d42546ac081462e0d86c37fa178b773
Jude Sajith <judesajith.aj@gmail.com>
2023-03-29 21:31:09 -0400

    ssss

--- pages/csv.py
+++ pages/csv.py
@@ -8,7 +8,7 @@ loader = PandasCSVReader()
 uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
 
 if uploaded_file is not None:
-    documents = loader.load_data(file=Path('./transactions.csv'))
+    documents = loader.load_data(file=uploaded_file)
     index = GPTSimpleVectorIndex.from_documents(documents)
     prompt = st.text_input("Enter you question")
     ans = index.query(prompt)

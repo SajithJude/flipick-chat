4dc22761dc3ed49082cc2142f9df0e4519a4bf19
Jude Sajith <judesajith.aj@gmail.com>
2023-03-29 21:54:45 -0400

    ssss

--- pages/csv.py
+++ pages/csv.py
@@ -1,24 +1,30 @@
 from pathlib import Path
 from llama_index import download_loader
-import streamlit as st
-import io
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
-PandasCSVReader = download_loader("PandasCSVReader")
+import streamlit as st 
+from llama_index import GPTSimpleVectorIndex
 
-loader = PandasCSVReader()
+PagedCSVReader = download_loader("PagedCSVReader")
 
-uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
+loader = PagedCSVReader()
 
-if uploaded_file is not None:
-    # Save the file to the local directory
-    file_path = f'transactions.csv'
-    with open(file_path, 'wb') as f:
-        f.write(uploaded_file.read())
 
-    # Load the data using the saved file
-    with open(file_path, 'r', encoding='ISO-8859-1') as f:
-        documents = loader.load_data(file=io.StringIO(f.read()))
-    index = GPTSimpleVectorIndex.from_documents(documents)
-    prompt = st.text_input("Enter your question")
-    ans = index.query(prompt)
-    st.write(ans)
+documents = loader.load_data(file=Path('./transactions.csv'))
+
+uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
+    if uploaded_file is not None:
+        file_path = f'transactions.csv'
+        with open(file_path, 'wb') as f:
+            f.write(uploaded_file.read())
+
+        # Load the data using the saved file
+        with open(file_path, 'r', encoding='ISO-8859-1') as f:
+            documents = loader.load_data(file=io.StringIO(f.read()))
+            index = GPTSimpleVectorIndex.from_documents(documents)
+
+quest = st.text_input("Please ask a question from the csv")
+if quest:
+    answ = index.query(quest)
+    st.write(answ)
+
+# documents = SimpleDirectoryReader('data').load_data()
+# index = GPTSimpleVectorIndex.from_documents(documents)

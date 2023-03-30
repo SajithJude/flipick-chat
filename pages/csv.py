a2b4a98a3445885232f49957fb27d3626871c87d
Jude Sajith <judesajith.aj@gmail.com>
2023-03-29 21:37:11 -0400

    ssss

--- pages/csv.py
+++ pages/csv.py
@@ -11,12 +11,12 @@ uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
 
 if uploaded_file is not None:
     # Save the file to the local directory
-    file_path = Path('./transactions.csv')
+    file_path = 'transactions.csv'
     with open(file_path, 'wb') as f:
         f.write(uploaded_file.read())
 
     # Load the data using the saved file
-    documents = loader.load_data(file=file_path)
+    documents = loader.load_data(file=Path(file_path))
     index = GPTSimpleVectorIndex.from_documents(documents)
     prompt = st.text_input("Enter your question")
     ans = index.query(prompt)

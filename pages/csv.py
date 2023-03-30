79ee22584a781fc3b99be0e39e665ada4b9ba039
Jude Sajith <judesajith.aj@gmail.com>
2023-03-29 21:32:44 -0400

    ssss

--- pages/csv.py
+++ pages/csv.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from llama_index import download_loader
 import streamlit as st
+import io
 PandasCSVReader = download_loader("PandasCSVReader")
 
 loader = PandasCSVReader()
@@ -8,7 +9,12 @@ loader = PandasCSVReader()
 uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
 
 if uploaded_file is not None:
-    documents = loader.load_data(file=uploaded_file)
+    # Read the bytes of the file and decode using UTF-8 encoding
+    file_bytes = uploaded_file.read()
+    file_string = file_bytes.decode("utf-8")
+
+    # Load the data using the decoded string
+    documents = loader.load_data(file=io.StringIO(file_string))
     index = GPTSimpleVectorIndex.from_documents(documents)
     prompt = st.text_input("Enter you question")
     ans = index.query(prompt)
